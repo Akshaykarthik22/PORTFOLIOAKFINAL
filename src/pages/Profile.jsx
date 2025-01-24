239f511cfb014ff23bd8ad3b6cbda8c9855e5f79
Akshay <jsakshaykarthik@gmail.com>
2025-01-24 10:16:04 +0530

    pudhusu

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -8,7 +8,7 @@ const Profile = () => {
             <div className='h-full w-full flex flex-col justify-center ml-6'>
                 <div className=' text-white w-[60%] h-[10%] my-5 font-extralight text-5xl span'>Hi! I'm <span className='text-yellow-400'>Akshay</span> </div>
                 {/* <div className=' text-teal-800 w-[60%] h-[10%] my-5 font-extralight text-5xl '>Akshay</div> */}
-                <div className=' w-[80%]  text-white h-[20%] my-5 font-thin text-3xl'>I am a Computer Science Engineering student with a passion for Python language, web development and AI. I've worked on diverse projects, including AI-based YouTube, Website, PDF summarizers and Telegram bots. Have intern experience in Bikal.AI and Hash Agile Technologies. </div>
+                <div className=' w-[80%]  text-white h-[20%] my-5 font-thin text-3xl'>I am a Computer Science Engineering student with a passion for Python language, Web development and AI. I've worked on diverse projects, including AI-based YouTube, Website, PDF summarizers and Telegram bots. Have intern experience in Bikal.AI and Hash Agile Technologies. </div>
             </div>
             <div className='h-full w-full flex justify-center items-center'>
                 <div className='h-[40%] w-[60%]  rounded-full'><img src={Img} className='rounded-full'></img></div>
